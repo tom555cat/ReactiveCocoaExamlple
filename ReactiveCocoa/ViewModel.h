aa8f13cb084a036cf0e8becc7265f38a8eba3192
BJTU-HT <bjtu-ht@163.com>
2017-09-05 00:24:08 +0800

    first commit

---
+++ ReactiveCocoa/ViewModel.h
@@ -0,0 +1,13 @@
+//
+//  ViewModel.h
+//  ReactiveCocoa
+//
+//  Created by tom555cat on 2017/9/4.
+//  Copyright © 2017年 Hello World Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewModel : NSObject
+
+@end
